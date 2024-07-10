a89231ed14da0721a8c13a100ce03439fddabc1a
selinyerli <141401610+selinyerli@users.noreply.github.com>
2024-07-10 11:43:24 +0300

    Initial commit 2

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FindObjectOfType<GameManager>().Explode();
+            
+        }
+    }
+}
